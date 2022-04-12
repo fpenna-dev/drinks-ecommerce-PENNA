62943b6b0b43e37c05220ada912c71198cb5c8e5
Facundo G Penna <78613226+fpenna-dev@users.noreply.github.com>
2022-04-12 02:10:36 -0300

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,45 @@ import React from "react";
 import "./navbar.css";
 import logo from "../logo.png";
 import CartWidget from "./CartWidget";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
       <div className="navbar">
-        <a href="" className="navbar__link">
+        <Link to="/" className="navbar__link">
           <img src={logo} alt="" className="navbar__logo" />
-        </a>
+        </Link>
         <ul className="navbar__categories">
           <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Categoria 1
-            </a>
+            <Link to="/category/2" className="navbar__link">
+              Fernet
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Categoria 2
-            </a>
+            <Link to="/category/14" className="navbar__link">
+              Aperitivos
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Categoria 3
-            </a>
+            <Link to="/category/17" className="navbar__link">
+              Vodka
+            </Link>
           </li>
           <li className="navbar__item">
-            <a href="" className="navbar__link">
-              Categoria 4
-            </a>
+            <Link to="/category/5" className="navbar__link">
+              Whiskies
+            </Link>
+          </li>
+          <li className="navbar__item">
+            <Link to="/category/3" className="navbar__link">
+              Licores
+            </Link>
+          </li>
+          <li className="navbar__item">
+            <Link to="/category/8" className="navbar__link">
+              Combos
+            </Link>
           </li>
           <li className="navbar__item">
             <CartWidget count="5" />
