70b84f2d33665a03936a5688674a53a285210df8
Facundo G Penna <78613226+fpenna-dev@users.noreply.github.com>
2022-04-14 10:00:00 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <ItemListContainer desc="Hola soy el componente ItemListContainer" />
-    </>
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:idCategory" element={<ItemListContainer />} />
+        <Route path="/item/:idItem" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
